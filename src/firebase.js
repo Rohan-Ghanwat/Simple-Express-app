37f4fa705bdafcf51fdd400b85422ad04bf149a5
Rohan-Ghanwat <112420469+Rohan-Ghanwat@users.noreply.github.com>
2024-07-12 14:52:28 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,10 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-adminsdk.json');
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://express-firebase-app-ddaa3.firebaseio.com'
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 
 const db = admin.firestore();
